973a7357bc5ee99e6213d4eb09a01c18c80b9e73
dipesh patel <dipesh@pateltechnologies.co.uk>
2025-01-13 11:34:42 +0000

    added and updated models

--- checkout-kata-cl/Models/PricingRules.cs
+++ checkout-kata-cl/Models/PricingRules.cs
@@ -4,24 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace checkout_kata.Models
+namespace checkout_kata_cl.Models
 {
     public class PricingRules
     {
         required
         public string _SKU { get; set; }
+        required
         public int _Quantity { get; set; }
+        required
         public double _Price { get; set; }
 
+        public List<PricingRules> Rules { get; set; }
+
+        /// <summary>
+        /// Returns all pricing rules
+        /// </summary>
         public PricingRules()
         {
 
         }
 
-        public getAllRules()
+        public List<PricingRules> getAllRules()
         {
-
+            return Rules;
         }
-
     }
 }
--- checkout-kata-cl/Models/Product.cs
+++ checkout-kata-cl/Models/Product.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace checkout_kata.Models
+namespace checkout_kata_cl.Models
 {
     public class Product
     {
         required
         public string _SKU { get; set; }
+        required
         public double _UnitPrice { get; set; }
 
+        public List<Product> Products { get; set; }
+
+        public Product() { }
+
+        /// <summary>
+        /// Returns all products
+        /// </summary>
+        /// <returns></returns>
+        public List<Product> getProducts()
+        {
+            return Products;
+        }
     }
 }
